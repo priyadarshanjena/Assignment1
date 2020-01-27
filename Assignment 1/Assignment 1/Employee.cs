119606a5af9ca234e8dfdb6c6d53ef27792584a0
priyadarshanjena <59999152+priyadarshanjena@users.noreply.github.com>
2020-01-27 06:44:10 -0800

    Add files via upload

---
+++ Assignment 1/Assignment 1/Employee.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Assignment_1
+{
+   
+        public class Employee
+        {
+            public delegate void MethodCall(String[] msg, int i);
+            public event MethodCall call;
+            public String[] s1 = new String[3];
+            public int i;
+
+            public void doAction()
+            {
+                call(s1, i);
+            }
+            private int Id;
+            private String EmpName, DeptName;
+
+            public Employee(int Id, String EmpName, String DeptName)
+            {
+                this.Id = Id;
+                this.EmpName = EmpName;
+                this.DeptName = DeptName;
+            }
+            public int GetId(Employee emp)
+            {
+                i = 0;
+                s1[0] = MethodBase.GetCurrentMethod().Name;
+                emp.call += new Employee.MethodCall(show_Action);
+                emp.doAction();
+                return this.Id;
+            }
+            public String GetName(Employee emp)
+            {
+                i = 1;
+                s1[1] = MethodBase.GetCurrentMethod().Name;
+                emp.call += new Employee.MethodCall(show_Action);
+                emp.call -= new Employee.MethodCall(show_Action);
+                emp.doAction();
+                return this.EmpName;
+            }
+
+            public string GetDeptName(Employee emp)
+            {
+                i = 2;
+                s1[2] = MethodBase.GetCurrentMethod().Name;
+                emp.call += new Employee.MethodCall(show_Action);
+                emp.call -= new Employee.MethodCall(show_Action);
+                emp.doAction();
+                return this.DeptName;
+            }
+            static void show_Action(String[] msg, int i)
+            {
+                System.Console.WriteLine(msg[i] + "method called");
+            }
+            public void Set(int UId)
+            {
+                String name = MethodBase.GetCurrentMethod().Name;
+                Id = UId;
+                System.Console.WriteLine("Updated Employee id is:" + Id);
+            }
+            public void Set(String UName, int UId)
+            {
+                String name = MethodBase.GetCurrentMethod().Name;
+                EmpName = UName;
+                System.Console.WriteLine("Update Employee Name is :" + EmpName);
+
+            }
+            public void Set(String UDeptName, String UName, int UId)
+            {
+                String name = MethodBase.GetCurrentMethod().Name;
+                DeptName = UDeptName;
+                System.Console.WriteLine("Updated Employee DepartmentName is:" + DeptName);
+            }
+
+        }
+    
+}
---
+++ Assignment 1/Assignment 1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Assignment_1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter employee Id :");
+            int id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Employee Name :");
+            String emp_name = Console.ReadLine();
+            Console.WriteLine("Enter Employee DepartmentName :");
+            String dept_name = Console.ReadLine();
+
+            Employee emp = new Employee(id, emp_name, dept_name);
+            int eid = emp.GetId(emp);
+            Console.WriteLine("Employee Id : {0}", eid);
+            String ename = emp.GetName(emp);
+            Console.WriteLine("Employee Name : {0}", ename);
+            String edept = emp.GetDeptName(emp);
+            Console.WriteLine("Employee DepartmentName : {0}", edept);
+
+            Console.WriteLine("Enter Employee Id to update : ");
+            int uid = Convert.ToInt32(Console.ReadLine());
+            emp.Set(uid);
+            Console.WriteLine("Enter Employee Name to update :");
+            String uname = Console.ReadLine();
+            emp.Set(uname, 0);
+
+            Console.WriteLine("Enter Employee DepartmentName To Update :");
+            String UdeptName = Console.ReadLine();
+            emp.Set(UdeptName, null, 0);
+
+        }
+    }
+   
+}
